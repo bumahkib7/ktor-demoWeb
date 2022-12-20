9db0cde2e85fa6cbe72bfa2467d6d9d4c618f8d8
Bukhari bin mahmoud Kibuka <93518580+bumahkib7@users.noreply.github.com>
2022-12-20 19:26:41 +0100

    initial

--- src/main/kotlin/com/codeWarrior/Application.kt
+++ src/main/kotlin/com/codeWarrior/Application.kt
@@ -6,10 +6,10 @@ import io.ktor.server.netty.*
 import com.codeWarrior.plugins.*
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+    embeddedServer(Netty, port = 4000, host = "localhost", module = Application::module)
         .start(wait = true)
 }
 
 fun Application.module() {
     configureRouting()
-}
+   }
--- src/main/kotlin/com/codeWarrior/plugins/Routing.kt
+++ src/main/kotlin/com/codeWarrior/plugins/Routing.kt
@@ -1,16 +1,27 @@
 package com.codeWarrior.plugins
 
-import io.ktor.server.routing.*
 import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.response.*
 import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
 
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+
+        var weather: String = "sunny"
+        get("/weather_forecast") {
+            call.respondText(weather, contentType = ContentType.Text.Plain)
+        }
+
+        post("/weather_forecast/post"){
+            weather = call.receiveText()
+            call.respondText("The weather has been set to $weather ",
+                contentType = ContentType.Any, status = HttpStatusCode.Accepted)
         }
     }
 }
